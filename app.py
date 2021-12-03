b4575c49744cb6362b81a7417d6a585fe28239f8
Daxin Niu <daxinniu111@gmail.com>
2021-12-03 22:39:48 +0000

    appupdate

--- app.py
+++ app.py
@@ -3,48 +3,69 @@ from dash import dcc
 from dash import html
 from dash import dash_table
 from dash.dependencies import Input, Output
+import dash_pivottable
 import plotly.express as px
 import pandas as pd
 
 df = pd.read_csv('https://bit.ly/elements-periodic-table')
+def identity(x): return x
+
+df2 = df.pivot_table(
+    index='Period',
+    columns='Group', 
+    values='Element',
+    aggfunc=identity,
+)
+df3 = pd.DataFrame(df2.to_records())
 
 app = dash.Dash(__name__)
+
 
 app.layout = html.Div([
     dcc.Dropdown(
         id="index-dropdown",
         options=[{'label': i, 'value': i} for i in df.columns],
-        multi=True,
-        value=df.columns
+        multi=False,
+        value="Period"
+    ),
+    dcc.Dropdown(
+        id="columns-dropdown",
+        options=[{'label': i, 'value': i} for i in df.columns],
+        multi=False,
+        value="Group"
+    ),
+    dcc.Dropdown(
+        id="values-dropdown",
+        options=[{'label': i, 'value': i} for i in df.columns],
+        multi=False,
+        value="Element"
     ),
     dash_table.DataTable(
     id='table',
-    columns=[{"name": i, "id": i} for i in df.columns],
-    data=df.to_dict('records'),
-)
+    columns=[{"name": i, "id": i} for i in df3.columns],
+    data=df3.to_dict('records'),
+    )
 ])
-#[{"label": i, "value": df[i]} for i in df.columns]
+
 @app.callback([
     Output('table', 'data'),
     Output('table', 'columns')
     ], 
-    [Input('index-dropdown', 'value')])
-def update_cols(cols):
-    df1 = df[cols]
-    out_cols = [{"name": i, "id": i} for i in cols]
-    return df1.to_dict('records'), out_cols
-'''
-@app.callback(
-    Output('graph-with-slider', 'figure'),
-    Input('year-slider', 'value'))
-def update_figure(selected_year):
-    filtered_df = df[df.year == selected_year]
+    [
+        Input('index-dropdown', 'value'),
+        Input('columns-dropdown', 'value'),
+        Input('values-dropdown', 'value'),
+    ])
+def update_cols(index_val, columns_val, values_val):
+    temp = df.pivot_table(
+        index=index_val,
+        columns=columns_val, 
+        values=values_val,
+        aggfunc=identity,
+    )
+    out_df = pd.DataFrame(temp.to_records())
+    out_cols = [{"name": i, "id": i} for i in out_df]
+    return out_df.to_dict('records'), out_cols
 
-    fig = px.scatter(filtered_df, x="gdpPercap", y="lifeExp",
-                     size="pop", color="continent", hover_name="country",
-                     log_x=True, size_max=55)
-    fig.update_layout(transition_duration=500)
-    return fig
-'''
 if __name__ == '__main__':
     app.run_server(debug=True)
